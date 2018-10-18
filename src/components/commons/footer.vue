f75eb7cb66bcd44effa781a761e5ed8131066a06
MountainC44 <1071011167@qq.com>
2018-10-18 19:16:28 +0800

    :sparkles: 实现路由跳转&&增加登陆界面

---
+++ src/components/commons/footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="footer">
+    <span>Copyright ©2018 {{name}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'MountainC44',
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#footer {
+  position: fixed;
+  bottom: 10px;
+  left: 0px;
+  right: 0px;
+  width: 220px;
+  height: 25px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  color: #36353575;
+  font-size: 15px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,72 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import ElementUI from 'element-ui';
+import Echarts from 'echarts';
+import 'element-ui/lib/theme-chalk/index.css'; // element-ui样式文件需要单独引入
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App';
 import router from './router';
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false; // 设置为 false 以阻止 vue 在启动时生成生产提示
+Vue.use(Vuex);
+Vue.use(ElementUI);
+Vue.prototype.$Echarts = Echarts;
+
+const store = new Vuex.Store({
+  state: {
+    NewMsg: {
+      Msgs: [{
+        title: '暂无消息',
+        content: '暂无消息！',
+        url: '#no_msg',
+        id: 'no_msg',
+      }],
+    },
+  },
+  mutations: {
+    modifyMsg(state, Obj) {
+      if (state.NewMsg.Msgs[0].id === 'no_msg') {
+        state.NewMsg.Msgs.shift();
+      }
+      const obj = {
+        title: Obj.title,
+        content: Obj.content,
+      };
+      obj.id = `Msg_${Obj.id}`;
+      obj.url = `#${obj.id}`;
+      state.NewMsg.Msgs.push(obj);
+    },
+  },
+  actions: {
+    // fetchMsg(context) {
+    //   $.ajax({
+    //     url: 'PHP/GetMsgs.php',
+    //     type: 'GET',
+    //     data: {},
+    //     dataType: 'json',
+    //     success(response) {
+    //       if (typeof response === 'string') {
+    //         response = JSON.parse(response);
+    //       }
+    //       $(response).each((k, v) => {
+    //         // console.log(v.id+v.title+v.content);
+    //         context.commit('modifyMsg', v);
+    //       });
+    //     },
+    //   });
+    // },
+  },
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  store,
+  // created() {
+  //   store.dispatch('fetchMsg');
+  // },
+  components: {
+    App,
+  },
   template: '<App/>',
 });
